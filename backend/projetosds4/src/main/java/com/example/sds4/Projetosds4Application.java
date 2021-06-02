afdbd9e5c731921d4322812deee115d56fcc81c1
ElielNascimento <nascimentob.eliel@gmail.com>
2021-06-02 09:10:30 -0300

    projeto criado

---
+++ backend/projetosds4/src/main/java/com/example/sds4/Projetosds4Application.java
@@ -0,0 +1,13 @@
+package com.example.sds4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetosds4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetosds4Application.class, args);
+	}
+
+}
---
+++ backend/projetosds4/src/test/java/com/example/sds4/Projetosds4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sds4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetosds4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
